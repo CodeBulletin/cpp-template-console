a1b8ab87ba2b77d397a38ee7e1e0c806a22ddc8d
Bhavesh Malhotra <bmalhotra736@gmail.com>
2026-01-23 21:39:28 +0530

    cli11, curl  fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 
 @PKGINCLUDE@
 
-int main() {
+int main(int argc, char** argv) {
     std::cout << "Project Name: " << PROJECT_NAME << std::endl;
     std::cout << "Version: " << PROJECT_VERSION << std::endl;
     std::cout << "Author: " << PROJECT_AUTHOR << std::endl;
 
     @CODEEXAMPLE@
+
+    return 0;
 }
